45da00b773cba1f197344fb1058328f6f362750e
pololo300 <84199795+pololo300@users.noreply.github.com>
2023-04-10 11:56:04 +0200

    Main

--- localSearchLT.h
+++ localSearchLT.h
@@ -9,6 +9,7 @@ vector<int> localSearch(Graph g, float r)
 
 
     //parametres simmulated annealing
+    float ratio_change = 0.01;
     int n = g.size();
     int max_iter = 100000;
     int iter = 0;
@@ -19,53 +20,44 @@ vector<int> localSearch(Graph g, float r)
     while (T > T_min && iter < max_iter)
     {
 
-        int vertex = rand() % n;
-        int vertex_found;
+        cout << S.size() << endl;
 
-        vector<int> S_new = S;
-
-        //SHA DE FER BE AIXO
-
-
-        //sha de calclar una aproximacio de la solucio que sigui valida
-        //el que hi ha no esta be
-        /* EL QUE HI HAVIA ABANS
-        if (find(S.begin(), S.end(), vertex) != S.end()) {
-            vertex_found = vertex;
-            S.erase(find(S.begin(), S.end(), vertex));
-        } else {
-            vertex_found = -1;
-            S.push_back(vertex);
-        }*/
+        vector<int> S_new;
 
         do {
-            //chanche S_new
-
+            //perturbation on S_new
+            S_new = S;
+            int to_change = rand () % int(n * ratio_change);
+            for (int i = 0; i < to_change; ++i)
+            {
+                int x = rand () % n;
+                //search the index of x in S
+                auto it = find(S_new.begin(), S_new.end(), x);
+                if (it != S_new.end()) 
+                    S_new.erase(it);
+                else S_new.push_back(x);
+            }
         } while ( LT(g, S_new, r) != n );
+
 
         int delta = (S_new.size() - S.size());;
         float p = exp(delta / T);
+
         
 
-        if (delta < 0 && (rand() / (float)RAND_MAX) < p)
+        if (delta > 0 && (rand() / (float)RAND_MAX) < p)
         {
-            cout << "rebutjat with probability: " << p << ' ';
-            // rebutjem
-            if (vertex_found == -1)
-            {
-                S.pop_back();
-            }
-            else
-            {
-                S.push_back(vertex_found);
-            }
+            //cout << "rebutjat with probability: " << p << ' ';
+
         }
         else
-        {
+        {   
+            S = S_new;
             cout << "acceptat with probability: " << p << ' ';
         }
         T *= alpha;
         iter++;
+        cout << S.size() << endl;    
     }
     cout << "T: " << T << endl;
     cout << "iter: " << iter << endl;
